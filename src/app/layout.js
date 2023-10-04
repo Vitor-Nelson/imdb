be08d311aa63ec4c911d986452ee371f54c9cfd9
Vitor Nelson <vitornelson.personal@gmail.com>
2023-10-04 17:32:08 +0100

    Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
+import Header from '@/app/Components/header'
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = {
   title: 'IMDb Clone Project',
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {/* Header */}
+        <Header />
+        
+        {children}
+      </body>
     </html>
   )
 }
