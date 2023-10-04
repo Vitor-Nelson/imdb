609a3e23e46e2a82acba3d0eac2a1662e87dc282
Vitor Nelson <vitornelson.personal@gmail.com>
2023-10-04 20:23:12 +0100

    Add Card Component

--- src/loading.jsx
+++ src/loading.jsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function loading() {
     return (
       <div className="flex justify-center">
-        <img className="h-96" src="../public/Rolling-1s-200px.svg" alt="loading..." />
+        <Image className="h-96" src="/public/Rolling-1s-200px.svg" alt="loading..."/>
       </div>
     );
   }
