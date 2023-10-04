0229a6e3079c3dd1fc941fe4f137d854ce89a19c
Vitor Nelson <vitornelson.personal@gmail.com>
2023-10-04 19:35:21 +0100

    Loading

---
+++ src/loading.jsx
@@ -0,0 +1,7 @@
+export default function loading() {
+    return (
+      <div className="flex justify-center">
+        <img className="h-96" src="../public/Rolling-1s-200px.svg" alt="loading..." />
+      </div>
+    );
+  }
